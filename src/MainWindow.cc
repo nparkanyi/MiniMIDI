9d7869dae90f312e0cb61dcc51c5e4feda938f7a
Nicholas Parkanyi <n.parkanyi@gmail.com>
2016-06-14 22:56:45 -0400

    playback buttons stay centred

--- src/MainWindow.cc
+++ src/MainWindow.cc
@@ -96,9 +96,16 @@ MainWindow::MainWindow() : Fl_Window(RES_X, RES_Y)
   menu = new Fl_Menu_Bar(0, 0, RES_X, 30);
   menu->copy(items);
 
+  //padding to keep the buttons centred
+  Fl_Group* left_pad = new Fl_Group(0, RES_Y - 50, RES_X / 2, 40);
+  left_pad->resizable(new Fl_Box(0, RES_Y - 50, RES_X / 2 - 70, 40));
+
   PlaybackControls* playback = new PlaybackControls(RES_X / 2 - 70, RES_Y - 50,
                                                     140, 40, view);
   playback->end();
+
+  Fl_Group* right_pad = new Fl_Group(RES_X / 2, RES_Y - 50, RES_X / 2, 40);
+  right_pad->end();
 
   end();
 }
